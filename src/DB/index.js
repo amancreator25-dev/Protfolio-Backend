47dd723a6d0eb41a37db27f75b447dbd3922b168
Aman <amancreator25@gmail.com>
2026-07-17 20:16:39 +0530

    fix: add logging for MongoDB URI in connectDB function

--- src/DB/index.js
+++ src/DB/index.js
@@ -3,6 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
+        console.log(process.env.MONGODB_URI)
         const uri = `${process.env.MONGODB_URI}/${DB_NAME}`;
 
         console.log(uri);
