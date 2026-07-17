526c6022630c81b3cd0a5f8d3cb79fa30a0fb4ce
Aman <amancreator25@gmail.com>
2026-07-17 20:10:25 +0530

    refactor: improve MongoDB connection logic and logging in connectDB function

--- src/DB/index.js
+++ src/DB/index.js
@@ -1,18 +1,41 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME || "Protfolio" }`)
+        const uri = `${process.env.MONGODB_URI}/${DB_NAME}`;
 
-      //  console.log(connectionInstance);
+        console.log(uri);
 
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(uri);
+
+        console.log(
+            `MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`
+        );
     } catch (error) {
-        console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
+        console.log("MONGODB connection FAILED", error);
+        process.exit(1);
     }
-}
+};
 
-export default connectDB
+export default connectDB;
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";
+
+
+// const connectDB = async () => {
+//     try {
+//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME || "Protfolio" }`)
+
+//       //  console.log(connectionInstance);
+
+//         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+//     } catch (error) {
+//         console.log("MONGODB connection FAILED ", error);
+//         process.exit(1)
+//     }
+// }
+
+// export default connectDB
